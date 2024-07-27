ce5ea380228ae2e73710d9af13fb10e9350840ec
vishwasbellani <vishwasbellani26439@gmail.com>
2024-07-27 09:54:49 +0530

    hello

---
+++ src/test/java/com/example/demo_application1/DemoApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_application1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
